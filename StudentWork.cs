619c45d8833df206f0019303df2c1fb7680ffe13
Mr C Millington <chris.millington@elliotthudsoncollege.ac.uk>
2024-09-14 18:18:50 +0100

    Whole

---
+++ StudentWork.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Variables
+{
+    public class Variables
+    {
+        public string PredictRun()
+        {
+            String greet = "Hello World";
+            Console.WriteLine(greet);
+            return greet;
+        }
+
+        public  String investigate1()
+        {
+            String one = "World";
+            String two = "Hello";
+            Console.WriteLine(two + one);
+            return two + one;
+        }
+
+        public  String investigate2()
+        {
+            String one = "5";
+            String two = "7";
+            Console.WriteLine(two + one);
+            return two + one;
+        }
+
+        public  String investigate3()
+        {
+            String one = "5";
+            String two = "7";
+            one = "Llama";
+            Console.WriteLine(two + one);
+            return two + one;
+        }
+
+        public  int investigate4()
+        {
+            int oneVariable = 5;
+            int twoVariable = 7;
+            Console.WriteLine("Investigate 4");
+            Console.WriteLine(twoVariable + oneVariable);
+            Console.WriteLine(twoVariable - twoVariable);
+            Console.WriteLine(twoVariable * oneVariable);
+            Console.WriteLine(twoVariable / oneVariable);
+            return (oneVariable + twoVariable);
+
+        }
+
+        public  double investigate5()
+        {
+            double oneVariable = 5;
+            double twoVariable = 7;
+            Console.WriteLine("Investigate 5");
+            Console.WriteLine(twoVariable + oneVariable);
+            Console.WriteLine(twoVariable - twoVariable);
+            Console.WriteLine(twoVariable * oneVariable);
+            Console.WriteLine(twoVariable / oneVariable);
+            return (oneVariable + twoVariable);
+        }
+
+        public  String investigate6()
+        {
+            double oneVariable = 5;
+            double twoVariable = 7;
+            Console.WriteLine("Investigate 6");
+            Console.WriteLine(oneVariable + " Party in the USA");
+            return (oneVariable + " Party in the USA");
+        }
+
+        public  String investigate7()
+        {
+            double oneVariable = 5;
+            Console.WriteLine("Investigate 7");
+            Console.WriteLine("What's your name ?");
+            String someText = Console.ReadLine();
+            Console.WriteLine(oneVariable + someText);
+            return (oneVariable + someText);
+        }
+
+        public  int investigate8()
+        {
+            Console.WriteLine("Investigate 8");
+            Console.WriteLine("Enter a number");
+            int oneVariable = Console.Read();
+            Console.WriteLine(oneVariable * 3);
+            return oneVariable * 3;
+        }
+
+        public  int investigate9()
+        {
+            Console.WriteLine("Investigate 9");
+            Console.WriteLine("Enter a number");
+            int oneVariable = int.Parse(Console.ReadLine());
+            Console.WriteLine(oneVariable * 3);
+            return oneVariable * 3;
+        }
+
+        public  String modify1()
+        {
+            Console.WriteLine("Write a program to add 16 to any number entered");
+            String num = Console.ReadLine();
+            Console.WriteLine(num + 16);
+            return num + 16;
+        }
+
+        public  String modify2()
+        {
+            Console.WriteLine("Whats your first name ?");
+            String firstName = Console.ReadLine();
+            String lastName = Console.ReadLine();
+            Console.WriteLine(firstName + lastName);
+            return firstName + lastName;
+        }
+
+        public  int modify3()
+        {
+            Console.WriteLine("Enter a Value");
+            int one = Console.Read();
+            Console.WriteLine("Enter a second value");
+            int two = Console.Read();
+            Console.WriteLine(one / two);
+            return one / two;
+        }
+        public  String make1()
+        {
+            return "";
+        }
+
+        public  String make2()
+        {
+            return "";
+        }
+
+        public  String make3()
+        {
+            return "";
+        }
+
+        public  String extension1()
+        {
+            return "";
+        }
+
+        public  String extension2()
+        {
+            return "";
+        }
+    }
---
+++ startingPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01Variables;
+
+namespace VariablesTask
+{
+    public class Variables
+    {
+
+        static void Main(string[] args)
+        {
+            //Do not change any code below this point
+
+            var SW = new Variables();
+            SW.PredictRun();
+            /*
+            investigate1();
+            investigate2();
+            investigate3();
+            investigate4();
+            investigate5();
+            investigate6();
+            investigate7();
+            investigate8();
+            investigate9();
+            modify1();
+            modify2();
+            modify3();
+            make1();
+            make2();
+            make3();
+            extension1();
+            extension2();   
+            */
+        }
+
+
+    }
+}
